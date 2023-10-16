79c1478e4d3517cee9c936a732e24162482aed60
C520S <71176236+C520S@users.noreply.github.com>
2023-10-16 21:52:24 +0300

    Tried using selenium, but it didn't work

--- src/data.collection.py
+++ src/data.collection.py
@@ -1,41 +1,66 @@
 import csv
-import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
+import chromedriver_autoinstaller
+
+# Install ChromeDriver if not installed and set up in PATH
+chromedriver_autoinstaller.install()
 
 # IMDb movie URL 
 url = "https://www.imdb.com/title/tt1375666/reviews/?ref_=tt_ql_2S"
 
+# Initialize a Chrome WebDriver
+driver = webdriver.Chrome()
+
 # Send an HTTP GET request to the IMDb page
-response = requests.get(url)
+driver.get(url)
 
-if response.status_code == 200:
-    soup = BeautifulSoup(response.text, 'html.parser')
+# Find the "Load More" button and click it until you have reviews
+while len(driver.find_elements(By.ID, 'load-more-trigger')) > 0:
+    load_more_button = driver.find_element(By.ID, 'load-more-trigger')
     
-    # Extract movie reviews, ratings, or other relevant data from the page
-   
-    scraped_data = []
-
-    review_elements = soup.find_all('div', class_='text show-more__control')
-    for review_element in review_elements:
-        review_text = review_element.get_text(strip=True)
-        scraped_data.append({"review_text": review_text})
+    # Wait until the button is clickable
+    wait = WebDriverWait(driver, 10)  # Adjust the timeout as needed
+    wait.until(EC.element_to_be_clickable((By.ID, 'load-more-trigger')))
     
-    # Specify the CSV file path
-    csv_file = "data/movie_reviews.csv"
+    # Scroll the button into view
+    driver.execute_script("arguments[0].scrollIntoView();", load_more_button)
+    
+    # Click the button
+    load_more_button.click()
 
-    # Create or open the CSV file for writing
-    with open(csv_file, mode="w", newline="", encoding="utf-8") as file:
-        # Define the CSV writer
-        writer = csv.writer(file)
+    # Wait for the new reviews to load 
+    driver.implicitly_wait(5)
 
-        # Write the header row
-        writer.writerow(["review_text"])
+# Once all reviews are loaded, parse the page using BeautifulSoup
+soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-        # Write the data rows
-        for data in scraped_data:
-            writer.writerow([data["review_text"]])
+scraped_data = []
+review_elements = soup.find_all('div', class_='text show-more__control')
+for review_element in review_elements:
+    review_text = review_element.get_text(strip=True)
+    scraped_data.append({"review_text": review_text})
 
-    print(f"Scraped {len(scraped_data)} reviews and saved to {csv_file}")
+# Close the WebDriver
+driver.quit()
 
-else:
-    print("Failed to retrieve the webpage. Status code:", response.status_code)
+# Specify the CSV file path
+csv_file = "data/movie_reviews.csv"
+
+# Create or open the CSV file for writing
+with open(csv_file, mode="w", newline="", encoding="utf-8") as file:
+    # Define the CSV writer
+    writer = csv.writer(file)
+
+    # Write the header row
+    writer.writerow(["review_text"])
+
+    # Write the data rows
+for data in scraped_data:
+    writer.writerow([data["review_text"]])
+
+
+print(f"Scraped {len(scraped_data)} reviews and saved to {csv_file}")
