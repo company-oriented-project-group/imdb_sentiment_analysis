1b029d83742d0bc8ff94dc9a6861e526dde5d17b
C520S <71176236+C520S@users.noreply.github.com>
2023-10-23 17:35:13 +0300

    Adding comments

--- src/text.preprocessing.py
+++ src/text.preprocessing.py
@@ -1,28 +1,28 @@
 import pandas as pd
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-from nltk.stem import SnowballStemmer  
+from nltk.stem import SnowballStemmer  # Import the Snowball Stemmer
 
 import nltk
 nltk.download('punkt')
 nltk.download('stopwords')
 
-
+# Read the movie reviews data
 df = pd.read_csv('movie_reviews.csv', encoding='utf-8')
 print(df.head())
 
-
+# Tokenize the review text
 df['review_text'] = df['review_text'].apply(lambda text: word_tokenize(text))
 
-
+# Initialize the Snowball Stemmer
 stemmer = SnowballStemmer('english')
 
-
+# Apply stemming to each token in the 'review_text'
 df['review_text'] = df['review_text'].apply(lambda tokens: [stemmer.stem(token) for token in tokens])
 
-
+# Remove stopwords
 stop_words = set(stopwords.words('english'))
 df['review_text'] = df['review_text'].apply(lambda tokens: [word for word in tokens if word.lower() not in stop_words])
 
-
+# Save the preprocessed data
 df.to_csv('preprocessed_review_texts.csv', index=False)
