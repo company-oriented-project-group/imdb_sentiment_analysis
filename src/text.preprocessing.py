e6c7331063d7e093c1281ab80ce88b4b02fc1521
C520S <71176236+C520S@users.noreply.github.com>
2023-10-24 15:08:09 +0300

    Adding comments

--- src/text.preprocessing.py
+++ src/text.preprocessing.py
@@ -6,13 +6,13 @@ import nltk
 nltk.download('punkt')
 nltk.download('stopwords')
 
-
+# Read the movie reviews data
 df = pd.read_csv('data/movie_reviews.csv', encoding='utf-8')
 print(df.head())
-
+# Tokenize the review text
 df['review_text'] = df['review_text'].apply(lambda text: word_tokenize(text))
-
+# Remove stopwords
 stop_words = set(stopwords.words('english'))
 df['review_text'] = df['review_text'].apply(lambda tokens: [word for word in tokens if word.lower() not in stop_words])
-
+# Save the preprocessed data
 df.to_csv('data/preprocessed_review_texts.csv', index=False)
