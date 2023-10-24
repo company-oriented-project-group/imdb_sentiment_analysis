dabf3f4552da53eb0bb7c43691d2b8de36d21881
C520S <71176236+C520S@users.noreply.github.com>
2023-10-24 12:22:19 +0300

    Try using LancasterStemmer to work out the bugs in data.

--- src/text.preprocessing.py
+++ src/text.preprocessing.py
@@ -1,21 +1,21 @@
 import pandas as pd
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-from nltk.stem import SnowballStemmer  # Import the Snowball Stemmer
+from nltk.stem import LancasterStemmer  # Import the Lancaster Stemmer
 
 import nltk
 nltk.download('punkt')
 nltk.download('stopwords')
 
 # Read the movie reviews data
-df = pd.read_csv('movie_reviews.csv', encoding='utf-8')
+df = pd.read_csv('data/movie_reviews.csv', encoding='utf-8')
 print(df.head())
 
 # Tokenize the review text
 df['review_text'] = df['review_text'].apply(lambda text: word_tokenize(text))
 
-# Initialize the Snowball Stemmer
-stemmer = SnowballStemmer('english')
+# Initialize the Lancaster Stemmer
+stemmer = LancasterStemmer()
 
 # Apply stemming to each token in the 'review_text'
 df['review_text'] = df['review_text'].apply(lambda tokens: [stemmer.stem(token) for token in tokens])
@@ -25,4 +25,4 @@ stop_words = set(stopwords.words('english'))
 df['review_text'] = df['review_text'].apply(lambda tokens: [word for word in tokens if word.lower() not in stop_words])
 
 # Save the preprocessed data
-df.to_csv('preprocessed_review_texts.csv', index=False)
+df.to_csv('data/preprocessed_review_texts.csv', index=False)
