7ed28980ab4fbf84b51a6ea53b130b3690369c72
Aliki Kapasakalidi <aliki02.kapasakalidi@gmail.com>
2023-10-30 17:09:18 +0200

    Started working with nltk

--- src/model.py
+++ src/model.py
@@ -6,6 +6,21 @@ import seaborn as sns
 plt.style.use('ggplot')
 
 import nltk
+nltk.download('averaged_perceptron_tagger')
+nltk.download('punkt')
+nltk.download('maxent_ne_chunker')
+nltk.download('words')
 
-df = pd.read_csv('./IMDB-example-dataset.csv')
+df = pd.read_csv('./data/preprocessed_review_texts.csv')
+
+print(df.shape)
 df.head()
+
+#the output is (500, 1) meaning there are 500 rows and 1 argument, the review
+
+example = df['review_text'][6]
+print(example)
+tokens = nltk.word_tokenize(example)
+tagged = nltk.pos_tag(tokens)
+entities = nltk.chunk.ne_chunk(tagged)
+entities.pprint()
